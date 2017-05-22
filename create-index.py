72ad3be232d856d9b26bd9e950a3e7205c790175
Niklas Morberg <(none)>
2017-05-22 15:33:30 +0200

    Hoppa över katalogen '.'

--- create-index.py
+++ create-index.py
@@ -7,6 +7,7 @@ print("# Morbergs receptsamling")
 
 for dirName, subdirList, fileList in os.walk(rootDir):
     if '.git' in subdirList: subdirList.remove('.git')
+    if (dirName=='.'): continue
     print("## {}".format(dirName[2:]))
     for fname in fileList:
         print("[{}]({}/{})".format(fname[:-3], dirName, fname))
