60e5193145a6a3fee1185520515c8e2355eff4b6
Niklas Morberg <niklas.morberg@gmail.com>
2018-06-11 16:11:08 +0200

    Sort category and recipe

--- create-index.py
+++ create-index.py
@@ -7,13 +7,15 @@ rootDir = '.'
 print("# Morbergs receptsamling")
 
 for dirName, subdirList, fileList in os.walk(rootDir):
+    subdirList.sort()
     if '.git' in subdirList:
         subdirList.remove('.git')
     if (dirName == '.'):
         continue
+    # normalize not needed for APFS introduced in macOS 10.13
     category = unicodedata.normalize('NFC', dirName)
     print("\n## {}\n".format(category[2:]))
-    for fname in fileList:
+    for fname in sorted(fileList):
         recipeLink = unicodedata.normalize('NFC', fname)
         with open(dirName + '/' + fname, 'r') as f:
                 recipeTitle = f.readline().strip('#').strip()
