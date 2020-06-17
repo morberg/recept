82388c52a390ac1c4e85980f1f245b9e26ded09f
Niklas Morberg <niklas.morberg@gmail.com>
2020-06-17 09:53:49 +0200

    Ignore .github dir

--- create-index.py
+++ create-index.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 import os
 
-rootDir = '.'
+rootDir = "."
 
 print("# Morbergs receptsamling")
 
 for dirName, subdirList, fileList in os.walk(rootDir):
     subdirList.sort()
-    if '.git' in subdirList:
-        subdirList.remove('.git')
-    if (dirName == '.'):
+    if ".git" in subdirList:
+        subdirList.remove(".git")
+    if ".github" in subdirList:
+        subdirList.remove(".github")
+    if dirName == ".":
         continue
     category = dirName
     print("\n## {}\n".format(category[2:]))
     for fname in sorted(fileList):
         recipeLink = fname
-        with open(dirName + '/' + fname, 'r') as f:
-                recipeTitle = f.readline().strip('#').strip()
+        with open(dirName + "/" + fname, "r") as f:
+            recipeTitle = f.readline().strip("#").strip()
         print("* [{}]({}/{})".format(recipeTitle, category, recipeLink))
