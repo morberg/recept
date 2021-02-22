5b11387b405e39abe9be26f3b46b22b0206c02f5
Niklas Morberg <niklas.morberg@gmail.com>
2021-02-22 17:42:03 +0100

    Large refactoring
    Should make code easier to understand

--- create-index.py
+++ create-index.py
@@ -1,25 +1,47 @@
 #!/usr/bin/env python3
 import os
 
-rootDir = "."
 
-print("# Morbergs receptsamling")
+def get_title(file_name):
+    """Extracts the recipe title from file_name.
 
-for dirName, subdirList, fileList in os.walk(rootDir):
-    subdirList.sort()
-    if ".git" in subdirList:
-        subdirList.remove(".git")
-    if ".github" in subdirList:
-        subdirList.remove(".github")
-    if dirName == ".":
-        continue
-    category = dirName
-    print("\n## {}\n".format(category[2:]))
-    for fname in sorted(fileList):
-        recipeLink = fname
-        with open(dirName + "/" + fname, "r") as f:
-            recipeTitle = f.readline().strip("#").strip()
-        print("* [{}]({}/{})".format(recipeTitle, category, recipeLink))
+    Assumes the title of the recipe is first line in file.
+    The first line can be formatted as a markdown header.
+    """
+    with open(file_name, "r") as f:
+        title = f.readline().strip("# \n")
+        return title
 
-print("")
+
+def get_file_list():
+    """Returns a sorted list of tuples with directories and
+    their file names from current directory.
+
+    Directories containing '.git' and current dir will be excluded.
+    """
+    file_list = [
+        (dir, files)
+        for dir, _, files in os.walk(".")
+        if ".git" not in dir  # exclude *.git* directories
+        if dir != "."  # exclude root dir
+    ]
+    file_list.sort()
+    return file_list
+
+
+def print_categories():
+    """Prints categories, recipe titles and links in markdown format."""
+    file_list = get_file_list()
+    for dir, files in file_list:
+        category = dir.strip("./")
+        print(f"## {category}\n")
+        for file in sorted(files):
+            file_name = dir + "/" + file
+            title = get_title(file_name)
+            print(f"* [{title}]({file_name})")
+        print()
+
+
+print("# Morbergs receptsamling\n")
+print_categories()
 print("## [Sous Vide](sous-vide.md)")
